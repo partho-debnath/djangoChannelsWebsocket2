756ba4469f5657a3419e985d688ec54f579390e9
Partho Debnath <71305747+pd28CSE@users.noreply.github.com>
2022-10-21 23:39:18 +0600

    client can send text messages. Realtime. No need to log in, the user name not be displayed
    using JsonWebsocketConsumer and AsyncJsonWebsocketConsumer generic websocket consumer

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,5 +1,6 @@
 from channels.generic.websocket import JsonWebsocketConsumer, AsyncJsonWebsocketConsumer
 
+from asgiref.sync import async_to_sync
 
 
 class MyJsonWebsocketConsumer(JsonWebsocketConsumer):
@@ -9,4 +10,60 @@ class MyJsonWebsocketConsumer(JsonWebsocketConsumer):
         print(self.channel_layer)
         print(self.channel_name)
         print(self.scope)
+        self.group_name =  self.scope['url_route']['kwargs']['groupName']
+
+        async_to_sync(self.channel_layer.group_add)(
+            self.group_name,
+            self.channel_name
+        )
         self.accept()
+    
+
+    def receive_json(self, content, **kwargs):
+        print('---------------Receive Dta---------------', content)
+
+        async_to_sync(self.channel_layer.group_send)(
+            self.group_name,
+            {
+                'type': 'send.messages',
+                'text': content
+            }
+        )
+    
+
+    def send_messages(self, event): # custom event handler, event is "send.messages"
+        self.send_json(event['text'])
+
+
+
+class MyAsyncJsonWebsocketConsumer(AsyncJsonWebsocketConsumer):
+
+    async def connect(self):
+        print('---------------Asynchronous Connection Opened.---------------')
+        print(self.channel_layer)
+        print(self.channel_name)
+        print(self.scope)
+        self.group_name =  self.scope['url_route']['kwargs']['groupName']
+
+        await self.channel_layer.group_add(
+            self.group_name,
+            self.channel_name
+        )
+        await self.accept()
+    
+
+    async def receive_json(self, content, **kwargs):
+        print('---------------Receive Data---------------', content)
+
+        await self.channel_layer.group_send(
+            self.group_name,
+            {
+                'type': 'send.messages',
+                'text': content
+            }
+        )
+    
+
+    async def send_messages(self, event): # custom event handler, event is "send.messages"
+        await self.send_json(event['text'])
+
--- chat/routing.py
+++ chat/routing.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import consumers
 
 websocket_urlpatterns = [ 
-    path('ws/chat/jsc/<str:groupName>/', consumers.MyJsonWebsocketConsumer.as_asgi()),
-
+    path('ws/chat/scj/<str:groupName>/', consumers.MyJsonWebsocketConsumer.as_asgi()),
+    path('ws/chat/acj/<str:groupName>/', consumers.MyAsyncJsonWebsocketConsumer.as_asgi())
 ]
